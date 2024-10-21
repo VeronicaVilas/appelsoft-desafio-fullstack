e71657b4b51847fa719e79f324c2836fdc61f293
Verônica Vilas <135287830+VeronicaVilas@users.noreply.github.com>
2024-10-21 09:31:41 -0300

    feat: adiciona tratamento global de erros

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -2,11 +2,14 @@
 
 namespace App\Exceptions;
 
+use App\Traits\HttpResponses;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use HttpResponses;
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +29,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e): \Symfony\Component\HttpFoundation\Response
+    {
+        if ($e instanceof \Illuminate\Validation\ValidationException) {
+            return $this->error($e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
+        return parent::render($request, $e);
     }
 }
